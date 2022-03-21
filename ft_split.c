9e79e3dea040e2db162809f138744e2f29d35347
atyanh <atyanh1999@gmail.com>
2022-03-21 11:59:47 +0000

    Split fix

--- ft_split.c
+++ ft_split.c
@@ -19,47 +19,69 @@ int	word_count(char	const *s, char c)
 	return (count);
 }
 
-char	*fill_str(char const *s, int len)
+char	*fill_str(char const *s, char c)
 {
 	char	*str;
 	int	i;
+	int	len;
 
 	i = 0;
-	str = (char *)malloc((len + 1) * sizeof(char));
-	while (len--)
+	len = 0;
+	while (s[i] != c || !s[i])
 	{
-		str[i++] = *s;
-		s++;
+		len++;
+		i++;
+	}
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (len--)		
+	{
+		str[i] = s[i];
+		i++;
 	}
 	str[i] = '\0';
 	return (str);
+}
+
+void	freemem(char **ptr)
+{
+	int	i;
+
+	i = 0;
+	while (*ptr[i])
+	{
+		free(ptr[i]);
+		i++;
+	}
+	free(ptr);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**ptr;
 	int	words;
-	int	len;
 	int	i;
 
 	i = 0;
 	words = word_count(s,c);
-	ptr = (char **)malloc((words + 1) * sizeof(char *));
-	if(!ptr)
+	if(!(ptr = (char **)malloc((words + 1) * sizeof(char *))))
 		return (NULL);
 	while (words--)
 	{
-		len = 0;
 		while (*s == c)
 			s++;
-		while (*s && *s != c)
+		ptr[i] = fill_str(s, c);
+		if (ptr[i] == NULL)
 		{
-			s++;
-			len++;
-			if (*s == c || !*s)
-				ptr[i++] = fill_str(s - len, len);
+			freemem(ptr);
+			return (NULL);
 		}
-	}
+		i++;
+		while (*s && *s != c)
+			s++;
+	}	
 	ptr[i] = (NULL);
 	return (ptr);
 }
